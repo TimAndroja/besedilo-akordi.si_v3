09ea3c31ad7f1a80ffb007acba53639efad968c2
TimAndroja <tim.androja@gmail.com>
2021-07-15 15:55:24 +0200

    sidebar set to static width

--- components/layout/LayoutMain.tsx
+++ components/layout/LayoutMain.tsx
@@ -6,12 +6,12 @@ import Sidebar from "./Sidebar";
 
 const Layout = (props: any) => (
     <div>
-        <div className="row  border-bottom">
-            <div className="col-md-2 border-end"><Logo /></div>
+        <div className="row  container-fluid d-flex border-bottom">
+            <div className="col-md-2 border-end" style={{ 'width': '245px' }}><Logo /></div>
             <div className="col-md-10"><Navbar /></div>
         </div>
-        <div className="row">
-            <div className="col-md-2 border-end"><Sidebar /></div>
+        <div className="row container-fluid d-flex">
+            <div className=" border-end" style={{ 'width': '245px' }}><Sidebar /></div>
             <div className="col-md-10 bg-light"> {props.children}</div>
         </div>
 
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -10,13 +10,13 @@ import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutline
 import Link from "next/link";
 export default function Sidebar() {
     return (
-        <aside className="py-5 ps-2 flex-fixed-width-itemcd Boben">
+        <aside className="py-5 ps-2 ">
             <div>
-                <div className="pb-4 "><Link href="/"><a><span className="px-3 h5 text-secondary"><HomeOutlinedIcon></HomeOutlinedIcon></span><span className="fw-bold  h6 text-secondary" >Domača stran</span></a></Link></div>
-                <div className="pb-4"><Link href="/"><a><span className="px-3 h5 text-secondary"><SchoolOutlinedIcon></SchoolOutlinedIcon></span><span className="fw-bold h6 text-secondary">Poučne Vsebine</span></a></Link></div>
-                <div className="pb-4"><Link href="/"><a><span className="px-3 h5 text-secondary"><PermContactCalendarOutlinedIcon></PermContactCalendarOutlinedIcon></span><span className="fw-bold h6 text-secondary" >Kontakt</span></a></Link></div>
-                <div className="pb-4"><Link href="/"><a><span className="px-3 h5 text-secondary"><PersonAddOutlinedIcon></PersonAddOutlinedIcon></span><span className="fw-bold h6 text-secondary">Dodaj Avtorja</span></a></Link></div>
-                <div className="pb-4"><Link href="/"><a><span className="px-3 h5 text-secondary"><LibraryMusicOutlinedIcon></LibraryMusicOutlinedIcon></span><span className="fw-bold h6 text-secondary">Dodaj Akorde</span></a></Link></div>
+                <div className="pb-4 "><Link href="/"><a><span className="px-2 h5 text-secondary"><HomeOutlinedIcon></HomeOutlinedIcon></span><span className="fw-bold  h6 text-secondary" >Domača stran</span></a></Link></div>
+                <div className="pb-4"><Link href="/"><a><span className="px-2 h5 text-secondary"><SchoolOutlinedIcon></SchoolOutlinedIcon></span><span className="fw-bold h6 text-secondary">Poučne Vsebine</span></a></Link></div>
+                <div className="pb-4"><Link href="/"><a><span className="px-2 h5 text-secondary"><PermContactCalendarOutlinedIcon></PermContactCalendarOutlinedIcon></span><span className="fw-bold h6 text-secondary" >Kontakt</span></a></Link></div>
+                <div className="pb-4"><Link href="/"><a><span className="px-2 h5 text-secondary"><PersonAddOutlinedIcon></PersonAddOutlinedIcon></span><span className="fw-bold h6 text-secondary">Dodaj Avtorja</span></a></Link></div>
+                <div className="pb-4"><Link href="/"><a><span className="px-2 h5 text-secondary"><LibraryMusicOutlinedIcon></LibraryMusicOutlinedIcon></span><span className="fw-bold h6 text-secondary">Dodaj Akorde</span></a></Link></div>
 
 
 
@@ -26,15 +26,15 @@ export default function Sidebar() {
             </div>
             <div>
                 <h6 className="ps-2 py-3 fw-bold">Promovirano</h6>
-                <div className="pb-4"><Link href="/"><a><span className="px-3 h5 text-secondary"><HelpOutlineOutlinedIcon></HelpOutlineOutlinedIcon></span><span className="fw-bold h6 text-secondary">Kako do promocije?</span></a></Link></div>
-                <div className="pb-4"><Link href="/"><a><span className="px-3 h5 text-secondary"><GradeOutlinedIcon></GradeOutlinedIcon></span><span className="fw-bold h6 text-secondary">Generate loop</span></a></Link></div>
+                <div className="pb-4"><Link href="/"><a><span className="px-2 h5 text-secondary"><HelpOutlineOutlinedIcon></HelpOutlineOutlinedIcon></span><span className="fw-bold h6 text-secondary">Kako do promocije?</span></a></Link></div>
+                <div className="pb-4"><Link href="/"><a><span className="px-2 h5 text-secondary"><GradeOutlinedIcon></GradeOutlinedIcon></span><span className="fw-bold h6 text-secondary">Generate loop</span></a></Link></div>
 
 
             </div>
             <div>
                 <h6 className="ps-2 py-3 fw-bold">Sponzorji</h6>
-                <div className="pb-4"><Link href="/"><a><span className="px-3 h5 text-secondary"><FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon></span><span className="fw-bold h6 text-secondary">Ansambel Objem</span></a></Link></div>
-                <div className="pb-4"><Link href="/"><a><span className="px-3 h5 text-secondary"><FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon></span><span className="fw-bold h6 text-secondary">Studio Boben</span></a></Link></div>
+                <div className="pb-4"><Link href="/"><a><span className="px-2 h5 text-secondary"><FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon></span><span className="fw-bold h6 text-secondary">Ansambel Objem</span></a></Link></div>
+                <div className="pb-4"><Link href="/"><a><span className="px-2 h5 text-secondary"><FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon></span><span className="fw-bold h6 text-secondary">Studio Boben</span></a></Link></div>
 
 
             </div>
